1a7b5c1cc5b35ecf2e93f78762b3e127f5439eb9
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-01-09 15:12:03 +0530

    update code require('fs/promises'); || reomove this [fs.readFile]

--- fs/fsPromises.js
+++ fs/fsPromises.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const fileName = "fsPromises.txt";
@@ -7,30 +7,30 @@ const filepath = path.join(__dirname, fileName);
 
 // syntext: fs.writeFile(path, data, options, callback)----
 //  file data created anf .txt file :----
-const writeFile = fs.promises.writeFile(filepath, "This is the initial Data", "utf-8",)
+const writeFile = fs.writeFile(filepath, "This is the initial Data", "utf-8",)
   .then(console.log("File created succesfully!"))
   .catch((err) => console.error(err));
 
 // file data read ------------------------
-// const readFile = fs.promises.readFile(filepath, "utf-8",)
+// const readFile = fs.readFile(filepath, "utf-8",)
 //   .then((data) => console.log("File has been Readed -: ", data))
 //   .catch((err) => console.error(err));
 
 
 // update code -----------------------
-// const appendFile = fs.promises.appendFile(filepath, "\nThis is the initial Data Updated", "utf-8",)
+// const appendFile = fs.appendFile(filepath, "\nThis is the initial Data Updated", "utf-8",)
 //   .then(console.log("File has been Updeted"))
 //   .catch((err) => console.error(err));
 
 // Delete code -----------------------
 
-// const deleteFile = fs.promises.unlink(filepath)
+// const deleteFile = fs.unlink(filepath)
 //   .then(console.log("File has been Deleted"))
 //   .catch((err) => console.error(err));
 
 // check Folder Files Path ----------------
 
 // const folderFilesPath = __dirname
-// const pathFile = fs.promises.readdir(folderFilesPath)
+// const pathFile = fs.readdir(folderFilesPath)
 //   .then((data) => console.log(data))
 //   .catch((err) => console.error(err));
