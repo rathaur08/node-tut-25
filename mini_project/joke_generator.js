9341d4e8b8489c536afe6835fd1aa369d9835c0c
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-01-20 14:45:48 +0530

    Create a Random Joke Generator app

---
+++ mini_project/joke_generator.js
@@ -0,0 +1,30 @@
+
+import https from "https";
+import chalk from "chalk";
+// import { JSON, response } from "express";
+
+const getJoke = () => {
+  const url = "https://official-joke-api.appspot.com/random_joke"
+
+  https.get(url, (response) => {
+    let data = "";
+    response.on(`data`, (chunk) => {
+      data += chunk
+    })
+
+    response.on('end', () => {
+      const joke = JSON.parse(data);
+      // console.log(joke);
+      console.log(`Here is a random (${joke.type}) joke`);
+      console.log(chalk.red(`${joke.setup}`));
+      console.log(chalk.blue.bgRed.bold(`${joke.punchline}`));
+    }) 
+
+    response.on(`error`, (err) => {
+      console.log(`Error facthing the joke, ${err.message}`);
+    })
+
+  })
+}
+
+getJoke()
