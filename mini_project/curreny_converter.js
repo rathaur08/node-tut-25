b7236f9ec367007e9025826f15bd170b4852a076
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-01-20 15:49:09 +0530

    add mini project file curreny_converter, weatherApp and some code

---
+++ mini_project/curreny_converter.js
@@ -0,0 +1,16 @@
+import https from "https";
+import readline from "readline";
+import chalk from "chalk";
+
+// Open Curreny Converter API Details 
+
+const apiKey = "";
+const url = ``;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+
+
---
+++ mini_project/weatherApp.js
@@ -0,0 +1,14 @@
+import readline from "readline/promises";
+import https from "https";
+import chalk from "chalk";
+
+// Open WeatherMap API Details 
+
+const API_KEY= "";
+const BASE_URL = ``;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
