173a6b21d5c177a00a34e821d009bbe45480592c
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-01-24 15:17:24 +0530

    Update Code Setup .env Files and Other Some Fix

--- express/app.js
+++ express/app.js
@@ -2,13 +2,10 @@ import express from "express";
 
 const app = express();
 
-
 app.get("/", (req, res) => res.send("Helllo World!"));
-
 app.get("/about", (req, res) => res.send("About Helllo World!"));
 
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at PORT: ${PORT}`);
 });
