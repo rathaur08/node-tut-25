19014b453aeee8e32e4edd23a735c778f273d070
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-01-24 14:50:57 +0530

    Create Express JS Server from Scratch

---
+++ express/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res) => res.send("Helllo World!"));
+
+app.get("/about", (req, res) => res.send("About Helllo World!"));
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT: ${PORT}`);
+});
