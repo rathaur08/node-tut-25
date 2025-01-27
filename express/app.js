4e403562e5da21587b5e8133d70eaad03f75919c
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-01-27 14:39:25 +0530

    Serve HTML CSS & JavaScript File

--- express/app.js
+++ express/app.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { PORT } from "./env.js";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Helllo World!"));
+app.get("/", (req, res) => {
+  
+  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+  res.sendFile(homePagePath);
+  
+});
+
 app.get("/about", (req, res) => res.send("About Helllo World!"));
 
 // const PORT = process.env.PORT || 3000;
