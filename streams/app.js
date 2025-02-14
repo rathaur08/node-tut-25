174ea7c6be90b2e3e1df341987eef6a185a92dc5
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-02-14 16:30:00 +0530

    Streams and Buffers| Simple File Copy Example with Events Code 1 WithOut Buffers code

---
+++ streams/app.js
@@ -0,0 +1,17 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+
+const inoutFilePath = path.join(import.meta.dirname, "input.txt");
+const outputFilePath = path.join(import.meta.dirname, "output.txt");
+
+const readableStream = createReadStream(inoutFilePath, {
+  encoding: "utf-8",
+  highWaterMark: 16,
+})
+
+const writableStream = createWriteStream(outputFilePath);
+
+readableStream.pipe(writableStream);
+
+readableStream.on("error", (err) => console.error("Error", err))
+writableStream.on("error", (err) => console.error("Error", err))
