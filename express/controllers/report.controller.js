1cdbc81dbfa8a60c4be5c5b9b888189c2160de4c
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-02-14 15:32:48 +0530

    added pages in controllers home , Contact and Other Some Fix

--- express/controllers/report.controller.js
+++ express/controllers/report.controller.js
@@ -1,5 +1,16 @@
-// import files links for your used
+import path from "path";
 import { student } from "../models/report.model.js";
+
+export const getHome = (req, res) => {
+  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+  res.sendFile(homePagePath);
+};
+
+export const getContact = (req, res) => {
+  console.log(req.body)
+  res.redirect("/")
+  // res.send(` contact page is -: ${req.query.name}, and Massage -: ${req.query.message}`)
+};
 
 export const reportCont = (req, res) => {
   // const student = [
--- express/routes/short.routes.js
+++ express/routes/short.routes.js
@@ -1,14 +1,15 @@
-import { readFile, writeFile } from "fs/promises"
 import path from "path";
 import { Router } from "express";
-import { reportCont } from "../controllers/report.controller.js"
+import { getHome, getContact, reportCont } from "../controllers/report.controller.js"
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
-  res.sendFile(homePagePath);
-});
+// router.get("/", (req, res) => {
+//   const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+//   res.sendFile(homePagePath);
+// });
+
+router.get("/", getHome);
 
 // app.get("/contact", (req, res) => {
 //   console.log(req.query)
@@ -16,11 +17,13 @@ router.get("/", (req, res) => {
 //   // res.send(` contact page is -: ${req.query.name}, and Massage -: ${req.query.message}`)
 // });
 
-router.post("/contact", (req, res) => {
-  console.log(req.body)
-  res.redirect("/")
-  // res.send(` contact page is -: ${req.query.name}, and Massage -: ${req.query.message}`)
-});
+router.post("/contact", getContact);
+
+// router.post("/contact", (req, res) => {
+//   console.log(req.body)
+//   res.redirect("/")
+//   // res.send(` contact page is -: ${req.query.name}, and Massage -: ${req.query.message}`)
+// });
 
 router.get("/about", (req, res) => res.send("About Page!"));
 
@@ -49,20 +52,11 @@ router.get("/profile/:username", (req, res) => {
   res.send(` Profile Name is : ${req.params.username}`)
 });
 
-router.get("/profile/:username/artical/:slug", (req, res) => {
+router.get("/blog/:blogname/artical/:slug", (req, res) => {
   console.log(req.params);
   const formatedSlig = req.params.slug.replace(/-/g, " ");
-  res.send(` Profile Name is : ${req.params.username} artical is ${req.params.slug}`);
+  res.send(` Profile Name is : ${req.params.blogname} artical is ${req.params.slug}`);
 });
-
-// // --------  404 Page Not Found ---------
-// router.use((req, res) => {
-//   // return res.status(404).send("<h1>404 Error <br/> Page Not Found!</h1>")
-//   // 2 Method Code File Path -----------
-//   return res
-//     .status(404)
-//     .sendFile(path.join(import.meta.dirname, "views", "404.html"))
-// });
 
 // default export
 // export default router;
