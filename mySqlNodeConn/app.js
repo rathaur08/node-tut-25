ea9505d2ddab370c4fa0109ea246f0b811926d99
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-02-18 12:39:00 +0530

    // Insert Data From Table || Using Inline Values || Using Prepared Statements || Using Many Values

--- mySqlNodeConn/app.js
+++ mySqlNodeConn/app.js
@@ -27,3 +27,26 @@ console.log("MySQL Connected Successfully");
 //     email varchar(100) not null unique
 //   )
 // `)
+
+
+//  4: is to perform CRUD Operation
+// insert Data 
+
+// Using Inline Values ( Not Recommended )
+// await db.execute(`
+// INSERT INTO users (username, email) 
+// VALUES ('Sunny','sunny@gmail.com');
+// `)
+
+// Using Prepared Statements ( Best Practice )
+// await db.execute(`INSERT INTO users (username, email) VALUES(?,?)`, [
+//   'Rahul', 'rahul@gmail.com'
+// ])
+
+const values = [
+  ['Ravi', 'ravi@gmail.com'],
+  ['Sumit', 'sumit@gmail.com'],
+  ['Bhola', 'bhola@gmail.com'],
+];
+
+await db.query(`INSERT INTO users (username, email) VALUES ?`, [values]);
