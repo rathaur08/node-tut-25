2cc4b0b40bc3933aac60f8e93ea7bd1228ff88d1
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-02-24 16:44:09 +0530

    Drizzle ORM || CRUD - Update Data usersTable using 2 Methods Querys
    Drizzle ORM || CRUD - Update Data usersTable 2 Methods Query

--- drizzle_mySql/app.js
+++ drizzle_mySql/app.js
@@ -1,5 +1,7 @@
 import { db } from "./config/db.js";
+import { eq } from 'drizzle-orm';
 import { usersTable } from "./drizzle/schema.js";
+
 
 const main = async () => {
   // 1 insert
@@ -20,10 +22,19 @@ const main = async () => {
   // console.log(" readUsers ", readUsers);
 
   // 4  Read Data Using Filter usersTable 
-  const readFilterUsers = await db.select().from(usersTable).where({ name: "Sunny" });
-  const readFilterUsers1 = await db.select().from(usersTable).where({ email: "sunny@gmail.com" });
-  console.log(" readFilterUsers ", readFilterUsers);
-  console.log(" readFilterUsers1 ", readFilterUsers1);
+  // const readFilterUsers = await db.select().from(usersTable).where({ name: "Sunny" });
+  // const readFilterUsers1 = await db.select().from(usersTable).where({ email: "sunny@gmail.com" });
+  // console.log(" readFilterUsers ", readFilterUsers);
+  // console.log(" readFilterUsers1 ", readFilterUsers1);
+
+  // 
+  // 4  Update usersTable Data
+  // const updateUser = await db.update(usersTable).set({ age: "24" }).where({ name: "Sunny" });
+  // console.log(" updateUser ", updateUser);
+
+  // 5  2-Method Update usersTable Data 
+  const updateUser2 = await db.update(usersTable).set({ age: "25" }).where(eq(usersTable.email, "sunny@gmail.com"));
+  console.log(" updateUser2 ", updateUser2);
 
 };
 
