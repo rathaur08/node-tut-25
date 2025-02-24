a89e8cc53006cfd33f2ec776615526d3ae1fdb39
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2025-02-24 16:16:20 +0530

    Drizzle ORM Code Update insert Multi Users Data

--- drizzle_mySql/app.js
+++ drizzle_mySql/app.js
@@ -3,9 +3,19 @@ import { usersTable } from "./drizzle/schema.js";
 
 const main = async () => {
   // 1 insert
-  const insertUser = await db.insert(usersTable)
-    .values({ name: "Sunny", age: "30", email: "sunny@gmail.com" })
-  console.log(" insertUser ", insertUser);
+  // const insertUser = await db.insert(usersTable)
+  //   .values({ name: "Sunny", age: "30", email: "sunny@gmail.com" })
+  // console.log(" insertUser ", insertUser);
+
+  // 2 insert Multi Users 
+  const insertMultiUsers = await db.insert(usersTable)
+    .values([
+      { name: "Rahul", age: "78", email: "rahul@gmail.com" },
+      { name: "Ravi", age: "34", email: "ravi@gmail.com" }
+    ])
+  console.log(" insertMultiUsers ", insertMultiUsers);
+
+
 };
 
 main().catch((err) => {
