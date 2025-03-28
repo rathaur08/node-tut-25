8fefa04d0f69e27fde2a7f88daf69b3123df5bcd
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-03-28 14:49:52 +0530

    Code Updated Users Logged In After Registration (Session + JWT) Auto

--- userRegisterLoginAuth/controllers/auth.controller.js
+++ userRegisterLoginAuth/controllers/auth.controller.js
@@ -43,9 +43,37 @@ export const postRegister = async (req, res) => {
   const hashedPassword = await hashPassword(password);
 
   const [user] = await createUser({ name, age, email, password: hashedPassword })
-  console.log("user", user);
+  // console.log("user", user);
 
-  res.redirect("/login")
+  // write a code Users Logged In After Registration 
+  // we need to create a sessions
+  const session = await createSession(user.id, {
+    ip: req.clientIp,
+    userAgent: req.headers["user-agent"],
+  })
+
+  const accessToken = createAccessToken({
+    id: user.id,
+    name: name,
+    email: email,
+    sessionId: session.id,
+  })
+
+  const refreshToken = createRefreshToken(session.id)
+
+  const baseConfig = { httpOnly: true, secure: true };
+
+  res.cookie("access_token", accessToken, {
+    ...baseConfig,
+    maxAge: ACCESS_TOKEN_EXPIRY,
+  });
+
+  res.cookie("refresh_token", refreshToken, {
+    ...baseConfig,
+    maxAge: REFRESH_TOKEN_EXPIRY,
+  });
+
+  res.redirect("/")
 };
 
 export const getLoginPage = (req, res) => {
