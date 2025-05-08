4a4d3f5142eccf5b03b4c1bd46dcc3005aa7168e
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-05-08 16:30:05 +0530

    Update Code And add new Functionality Complete Login/Register with GitHub OAuth

--- userRegisterLoginAuth/config/env.js
+++ userRegisterLoginAuth/config/env.js
@@ -3,8 +3,8 @@ import { z } from "zod";
 const envSchema = z.object({
   GOOGLE_CLIENT_ID: z.string().min(1),
   GOOGLE_CLIENT_SECRET: z.string().min(1),
-  // GITHUB_CLIENT_ID: z.string().min(1),
-  // GITHUB_CLIENT_SECRET: z.string().min(1),
+  GITHUB_CLIENT_ID: z.string().min(1),
+  GITHUB_CLIENT_SECRET: z.string().min(1),
   FRONTEND_URL: z.string().url().trim().min(1),
 });
 
--- userRegisterLoginAuth/routes/auth.routes.js
+++ userRegisterLoginAuth/routes/auth.routes.js
@@ -41,6 +41,9 @@ router.route("/reset-password/:token")
 router.route("/google").get(authControllers.getGoogleLoginPage);
 router.route("/google/callback").get(authControllers.getGoogleLoginCallback);
 
+router.route("/github").get(authControllers.getGithubLoginPage);
+router.route("/github/callback").get(authControllers.getGithubLoginCallback);
+
 router.route("/logout").get(authControllers.userLogoutPage);
 
 export const authRoute = router;
